04509cef0f1ecfd01e73e2a3061d77b4b9cddc79
Gcy255 <1295855884@qq.com>
2023-10-28 17:38:19 +0800

    111
    111

---
+++ Assets/Scripts/Player/Player_Feedback.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+using UnityEngine.Events;
+using Hmxs.Toolkit.Base.Singleton;
+using Unity.VisualScripting;
+
+public class Player_Feedback : MonoBehaviour
+{
+    public MMFeedbacks getBall;
+    public MMFeedbacks hurt;
+    public MMFeedbacks lostBall;
+    public MMFeedbacks peak;
+
+    void Update()
+    {
+
+    }
+}
